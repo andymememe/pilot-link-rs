09d04615788de0eee0ce86d1965dcd6746632ce3
Chen, Liang Yu (Andymememe) <andymememe@gmail.com>
2021-02-06 16:52:20 +0800

    Fix: Restart project with base of jSyncManager

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-extern crate num;
-#[macro_use] extern crate num_derive;
-
-pub mod pisock;
-pub mod pisync;
--- src/pisock/padp.rs
+++
@@ -1,5 +0,0 @@
-pub const PADP_DATA: i32 = 0x01;
-pub const PADP_WAKE: i32 = 0x101;
-pub const PADP_ACK: i32 = 0x02;
-pub const PADP_TICKLE: i32 = 0x04;
-pub const PADP_ABORT: i32 = 0x08;
